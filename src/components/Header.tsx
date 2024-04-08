34494b2f722fe3b99f5644805c299466c1598633
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-08 16:05:49 +0200

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex flex-col items-center p-12 m-4 bg-white border-2 border-black">
+      <div className="text-5xl font-bold">ACME Corporation</div>
+      <div className="text-3xl font-bold">Toscana</div>
+    </header>
+  );
+};
+
+export default Header;
