e658292ca76ca68591edf09f56224477ef46aa3d
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-04-08 16:07:40 +0200

    Update App component to include all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/Header';
+import MainNav from './components/MainNav';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Products from './components/Products';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Header />
+      <MainNav />
+      <main className="m-4 p-4 bg-white border-2 border-black">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chi-siamo" element={<About />} />
+          <Route path="/prodotti" element={<Products />} />
+          <Route path="/contatti" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <section className="flex flex-col items-center m-4">
+        <button
+          className="underline text-blue-700 font-semibold"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          Torna all'inizio
+        </button>
+      </section>
+    </>
   );
 }
 
